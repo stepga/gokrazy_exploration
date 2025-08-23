56fb05ee96f6616c192ae4df80467cc52603acf7
Stephan Gabert <stepga@nirgendwo.eu>
2025-08-23 09:48:54 +0200

    example.go: use https://gokrazy.org/userguide/permanent-data/ example

--- example/example.go
+++ example/example.go
@@ -1,4 +1,51 @@
 package main
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+)
+
+const config = "/perm/my-example/config.json"
+
+type Config struct {
+	FilesToCreate int `json:"files_to_create"`
+}
+
 func main() {
+	_, err := os.Stat(config)
+	if err != nil {
+		fsErr := &fs.ErrNotExist
+		if !errors.As(err, fsErr) {
+			panic(err)
+		}
+
+		content, err := json.Marshal(&Config{FilesToCreate: 4})
+		if err != nil {
+			panic(err)
+		}
+
+		if err = ioutil.WriteFile(config, content, 0600); err != nil {
+			panic(err)
+		}
+	}
+
+	content, err := ioutil.ReadFile(config)
+	if err != nil {
+		panic(err)
+	}
+
+	var c Config
+	if err := json.Unmarshal(content, &c); err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < c.FilesToCreate; i++ {
+		if err := ioutil.WriteFile(fmt.Sprintf("/perm/my-example/%d.txt", i), []byte("gokrazy rocks"), 0600); err != nil {
+			panic(err)
+		}
+	}
 }
